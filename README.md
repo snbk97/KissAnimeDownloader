9adb92efce5d17d704696dbaeac35fb3206f944c
snbk97 <snbk97@gmail.com>
2016-07-15 18:26:44 +0530

    misc.

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # KissAnimeDownloader
 A python utility to extract download links for animes on kissanime.to
 
+#Pre-requisite:
+    npm install phantomjs-prebuilt
 #Usage:
     python KA.py http://kissanime.to/Anime/<anime-name>
     
 #Troubleshooting:
 
   On any error, please restart the script
+
+#Credits:
+[Ashish Bansal](https://github.com/Ashish-Bansal) for helping and debugging 
--- csv2html.py
+++ csv2html.py
@@ -1,35 +1,52 @@
-# for explicit calling only
 def c2h(url):
     anime_name = url.split('http://kissanime.to/Anime/')[1]
-    fo = open('anime_links.csv', 'rb')
-    fh = open('anime_links.html', 'wb')
+    fo = open('anime_links.csv', 'rb')  # csv file
+    fh = open('anime_links.html', 'wb')  # html fileb')
 
     fh_pre_data = '''
-                <html>
-                <head>
-                <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-                <style>
-                body{
-                font-family:'Raleway',Arial;
-                }
-                </style>
-                </head>
+<html>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+<style>
+body{
 
-                <body>
-                <center><h1> <a href="https://github.com/snbk97/KissAnimeDownloader">Kiss Anime Downloader</a></h1></center>
-                <hr></br>
+font-family:'Raleway',Arial;
+color:green;
+}
+td{
+    padding:10px
+
+}
+#main_table{
+    -webkit-box-shadow: 0px 13px 21px -7px rgba(0,0,0,0.6);
+    -moz-box-shadow: 0px 13px 21px -7px rgba(0,0,0,0.6);
+    box-shadow: 0px 13px 21px -7px rgba(0,0,0,0.6);
+    border-radius:12px;
+}
+</style>
+</head>
+
+<body>
+<center><h1> <a href="https://github.com/snbk97/KissAnimeDownloader">Kiss Anime Downloader</a></h1></center>
+<hr></br>
+
                 '''
     fh.write(fh_pre_data)
     fh.write("<center><h2><a href=" + url + ">" + anime_name + "</a></h2><br>")
-
+    fh.write('<table id="main_table">')
     l = len(fo.readlines()) - 1
     fo.seek(0, 0)
     for i in fo.readlines()[l::-1]:
         ep = i.split('~')[::2][0]
         link = i.split('~')[::-1][0]
-        final = "EP " + ep + ': ' + '<a href=' + '"' + link + '"' + '>Link</a>'
+        final = "<td>" + ep + '<td><td>' + '<a href=' + \
+            '"' + link + '"' + '>Link</a></td>'
+        fh.write("<tr>")
         fh.write(final)
-        fh.write('</br>')
+        fh.write("</tr>")
 
+    fh.write("</table></center></body></html>")
     fh.close()
     fo.close()
+
+# c2h(url) #test
